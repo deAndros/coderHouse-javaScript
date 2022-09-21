984d8bddc5099f770412a0467eaac19457bcd28e
Andrés Gabriel Ramos <aramos@modernizacion.gob.ar>
2022-09-21 01:15:46 -0300

    Se añadió posibilidad de armar al equipo rival

--- index.js
+++ index.js
@@ -30,19 +30,6 @@ console.log(this);
 
 alert("Bienvenido a Andros Team Builder (ALFA version)!")
 
-function selectEvent(){
-    let event = prompt("Seleccione qué tipo de evento desea planificar \n1 - Fútbol 5\n2 - Basquet (3v3)")
-
-    switch(event){
-        case("1"):
-            alert(buildTeam(5))
-            break;
-        case("2"):
-            alert(buildTeam(3))
-            break;
-    }
-}
-
 function buildTeam(playerAmount){
     let teamPlayers = ""
     let teamName = prompt("Ingrese el nombre del equipo")
@@ -62,6 +49,28 @@ function buildTeam(playerAmount){
     
     console.log(teamName + ":\n" + teamPlayers)
     return teamName + ":\n" + teamPlayers
+}
+
+function selectEvent(){
+    let event = prompt("Seleccione qué tipo de evento desea planificar \n1 - Fútbol 5\n2 - Basquet (3v3)")
+
+    switch(event){
+        case("1"):
+            if(prompt("¿Desea planificar al equipo rival?\n1 - Sí\n2 - No") == "1"){
+                alert("Equipo 1 - " + buildTeam(5) + "\nVS\nEquipo 2 - " + buildTeam(5))
+            } else {
+                buildTeam(5)
+            }
+            break;
+            
+        case("2"):
+            if(prompt("¿Desea planificar al equipo rival?\n1- Sí\n2 - No") == "1"){
+                alert("Equipo 1 - " + buildTeam(3) + "\nVS\nEquipo 2 - " + buildTeam(3))
+            } else {
+                buildTeam(3)
+            }
+            break;
+    }
 }
 
 selectEvent()
